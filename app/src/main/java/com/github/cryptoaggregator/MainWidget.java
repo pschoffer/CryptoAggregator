bddbb6fbef00263e6cf90f2449cbf67cdd9c825f
Pavel Schoffer <xschof00@gmail.com>
2018-02-11 12:40:31 +0100

    Removes config and adds some more playing around

--- app/src/main/java/com/github/cryptoaggregator/MainWidget.java
+++ app/src/main/java/com/github/cryptoaggregator/MainWidget.java
@@ -4,6 +4,8 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.widget.RemoteViews;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 /**
  * Implementation of App Widget functionality.
@@ -14,11 +16,16 @@ public class MainWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = MainWidgetConfigureActivity.loadTitlePref(context, appWidgetId);
+//        CharSequence widgetText = MainWidgetConfigureActivity.loadTitlePref(context, appWidgetId);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.main_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setTextViewText(R.id.appwidget_text, "BTC");
 
+
+
+        RemoteViews record = new RemoteViews(context.getPackageName(), R.layout.main_record);
+        record.setTextViewText(R.id.record, "BTC");
+        views.addView(R.id.label_btc, record);
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
