0907c845539fdcdfa9bcc060952a46b54a47c1b7
Pavel Schoffer <xschof00@gmail.com>
2018-03-21 07:59:40 +0100

    Refresh on touch works

--- app/src/main/java/com/github/cryptoaggregator/service/widget/WidgetService.java
+++ app/src/main/java/com/github/cryptoaggregator/service/widget/WidgetService.java
@@ -1,10 +1,12 @@
 package com.github.cryptoaggregator.service.widget;
 
 import android.app.IntentService;
+import android.appwidget.AppWidgetManager;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
+import com.github.cryptoaggregator.MainWidget;
 import com.github.cryptoaggregator.util.Logger;
 
 /**
@@ -29,5 +31,11 @@ public class WidgetService extends IntentService {
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
         Logger.info("Handling intent from widget.");
+
+        final int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+
+        Logger.info("Triggering Update for widget " + appWidgetId);
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        MainWidget.updateAppWidget(this, appWidgetManager, appWidgetId);
     }
 }
