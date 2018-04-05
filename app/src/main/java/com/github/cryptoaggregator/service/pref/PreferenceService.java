39e3067c415a8358857118647333b1507a6d5b50
Pavel Schoffer <xschof00@gmail.com>
2018-04-05 07:54:05 +0200

    Loading of global prefs

--- app/src/main/java/com/github/cryptoaggregator/service/pref/PreferenceService.java
+++ app/src/main/java/com/github/cryptoaggregator/service/pref/PreferenceService.java
@@ -11,9 +11,12 @@ import com.github.cryptoaggregator.util.Logger;
  */
 
 public class PreferenceService {
-    private static final String PREFS_NAME = "com.github.cryptoaggregator.MainWidget";
+    private static final String WIDGET_PREFS_NAME = "com.github.cryptoaggregator.MainWidget";
+    private static final String GLOBAL_PREFS_NAME = "com.github.cryptoaggregator.Global";
     private static final String ENABLE_PREF_PREFIX = "ENABLE_COIN_";
-    private static final String COINS_ORDER = "COINS_ORDER_";
+    private static final String COINS_ORDER_PREF_PREFIX = "COINS_ORDER_";
+    private static final String ACTIVE_COINS_PREF = "ACTIVE_COINS";
+
     private static final String DEFAULT_COINS = "bitcoin|ethereum";
     private static final String COINS_SEPARATOR = "|";
 
@@ -25,7 +28,7 @@ public class PreferenceService {
 
     public void persistWidgetPreferences(int appWidgetId, WidgetPreferences preferences) {
         Logger.info("Persisting preferences for widget " + appWidgetId);
-        SharedPreferences.Editor prefs = context.getSharedPreferences(PREFS_NAME, 0).edit();
+        SharedPreferences.Editor prefs = context.getSharedPreferences(WIDGET_PREFS_NAME, 0).edit();
 
         final StringBuilder coins = new StringBuilder();
         for (String coin : preferences.getCurrencies()) {
@@ -45,7 +48,7 @@ public class PreferenceService {
         Logger.info("Loading preferences for widget " + appWidgetId);
         final WidgetPreferences widgetPreferences = new WidgetPreferences();
 
-        final SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, 0);
+        final SharedPreferences prefs = context.getSharedPreferences(WIDGET_PREFS_NAME, 0);
         final String coins = prefs.getString(getCoinOrderKey(appWidgetId), DEFAULT_COINS);
         for (String coin : coins.split("\\|")) {
             final boolean enabled = prefs.getBoolean(getCoinEnabledKey(appWidgetId, coin), false);
@@ -58,7 +61,7 @@ public class PreferenceService {
     public void clearWidgetPreferences(int appWidgetId) {
         Logger.info("Clearing preferences for widget " + appWidgetId);
 
-        final SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, 0);
+        final SharedPreferences prefs = context.getSharedPreferences(WIDGET_PREFS_NAME, 0);
         final String coins = prefs.getString(getCoinOrderKey(appWidgetId), "");
 
         final SharedPreferences.Editor prefsEditor = prefs.edit();
@@ -70,9 +73,22 @@ public class PreferenceService {
         prefsEditor.apply();
     }
 
+    public GlobalPreferences loadGlobalPreferences() {
+        Logger.info("Loading global preferences");
+        final GlobalPreferences globalPreferences = new GlobalPreferences();
+
+        final SharedPreferences prefs = context.getSharedPreferences(GLOBAL_PREFS_NAME, 0);
+        final String coins = prefs.getString(ACTIVE_COINS_PREF, DEFAULT_COINS);
+        for (String coin : coins.split("\\|")) {
+            globalPreferences.addCurrency(coin);
+        }
+
+        return globalPreferences;
+    }
+
     @NonNull
     private String getCoinOrderKey(int appWidgetId) {
-        return COINS_ORDER + appWidgetId;
+        return COINS_ORDER_PREF_PREFIX + appWidgetId;
     }
 
     private String getCoinEnabledKey(int appWidgetId, String coin) {
